9c303262b21321f705b448f3ff2d9b8364b98762
jpuri <jyotipuri@gmail.com>
2022-01-19 15:49:33 +0530

    fixes

--- ui/hooks/useTransactionMetrics.js
+++ ui/hooks/useTransactionMetrics.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 
 import { useGasFeeContext } from '../contexts/gasFee';
 import { MetaMetricsContext } from '../contexts/metametrics';
@@ -6,35 +6,40 @@ import { MetaMetricsContext } from '../contexts/metametrics';
 export const useTransactionMetrics = () => {
   const { transaction } = useGasFeeContext();
   const metricsEvent = useContext(MetaMetricsContext);
-  const { origin, type } = transaction;
 
-  const captureTransactionMetrics = ({ action, name, variables }) => {
-    metricsEvent({
-      eventOpts: {
-        category: 'Transactions',
+  const captureTransactionMetrics = useCallback(
+    ({ action, name, variables }) => {
+      if (!transaction) {
+        return;
+      }
+      const { origin, type } = transaction;
+      metricsEvent({
+        eventOpts: {
+          category: 'Transactions',
+          action,
+          name,
+        },
+        customVariables: {
+          origin,
+          transaction_type: type,
+          source: origin === 'metamask' ? 'user' : 'dapp',
+          ...variables,
+        },
+      });
+    },
+    [metricsEvent, transaction],
+  );
+
+  const captureTransactionMetricsForEIP1559V2 = useCallback(
+    ({ action, name, variables }) => {
+      captureTransactionMetrics({
         action,
         name,
-      },
-      customVariables: {
-        origin,
-        transaction_type: type,
-        source: origin === 'metamask' ? 'user' : 'dapp',
-        ...variables,
-      },
-    });
-  };
-
-  const captureTransactionMetricsForEIP1559V2 = ({
-    action,
-    name,
-    variables,
-  }) => {
-    captureTransactionMetrics({
-      action,
-      name,
-      variables: { ...variables, EIP_1559_V2: true },
-    });
-  };
+        variables: { ...variables, EIP_1559_V2: true },
+      });
+    },
+    [captureTransactionMetrics],
+  );
 
   return {
     captureTransactionMetrics,
